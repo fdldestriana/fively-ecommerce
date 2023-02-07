e23f7590133e242890b693c5ef3a17cce25d6461
fadlydestriana <fadlydestriana@gmail.com>
2023-02-07 20:42:35 +0700

    new commit

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -22,13 +22,9 @@ class User {
       id: json['id'],
       username: json['username'],
       email: json['email'],
-      firstName: (json['firstName'] != null) ? json['firstName'] : 'first name',
-      lastName: (json['lastName'] != null) ? json['lastName'] : 'last name',
-      gender: (json['gender'] != null) ? json['gender'] : 'male',
-      image: (json['image'] != null)
-          ? json['image']
-          : 'https://robohash.org/autquiaut.png?size=50x50&set=set1',
-      token: (json['token'] != null)
-          ? json['token']
-          : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvYXV0cXVpYXV0LnBuZz9zaXplPTUweDUwJnNldD1zZXQxIiwiaWF0IjoxNjM1NzczOTYyLCJleHAiOjE2MzU3Nzc1NjJ9.n9PQX8w8ocKo0dMCw3g8bKhjB8Wo7f7IONFBDqfxKhs');
+      firstName: json['firstName'] ?? 'first name',
+      lastName: json['lastName'] ?? 'last name',
+      gender: json['gender'] ?? 'male',
+      image: json['image'] ?? 'image',
+      token: json['token'] ?? 'token');
 }
